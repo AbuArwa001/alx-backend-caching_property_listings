a1a77fa8c52f1be64cb37c8c733bab9f43a99b61
khalfan <AbuuArwa001>
2025-07-13 05:06:59 +0300

    feat: implement cache invalidation on property save and delete signals

--- properties/apps.py
+++ properties/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        # Import signals to ensure they are registered
+        import properties.signals
+        print("Properties app is ready and signals are imported.")
